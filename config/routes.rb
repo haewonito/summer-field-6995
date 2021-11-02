ece3e8d595bd5a459ac6d1572a11081493e0f331
Haewon Jeon <haewonito@gmail.com>
2021-11-02 10:38:51 -0600

    test for us3

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/studios", to: "studios#index"
 
   get "/movies/:id", to: "movies#show"
+  
 
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -33,11 +33,26 @@ RSpec.describe "movies show page", type: :feature do
   it "shows average age of all of the movie's actors" do
     expect(page).to have_content("66.5")
   end
+
+  it "does not show any actors that are not part of the movie" do
+    expect(page).to_not have_content(@emma.name)
+  end
+
+  it "has a form to add an actor to this movie" do
+    expect(page).to have_content("Fill in the name of the actor you want to add to the movie and click 'Submit'")
+    fill_in :actor_name, with: @emma.name
+    click_on "Submit"
+
+    expect(current_path).to eq("/movies/#{@shrek.id}")
+    expect(page).to have_content("@emma.name")
+  end
 end
 
-#
-# As a user,
-# When I visit a movie's show page.
-# I see the movie's title, creation year, and genre,
-# and a list of all its actors from youngest to oldest.
-# And I see the average age of all of the movie's actors
+
+# Add an Actor to a Movie
+
+# And I see a form to add an actor to this movie
+# When I fill in the form with the name of an actor that exists in the database
+# And I click submit
+# Then I am redirected back to that movie's show page
+# And I see the actor's name is now listed
