4a4e3b717972c2e04f89eaf012644e83364307bd
Haewon Jeon <haewonito@gmail.com>
2021-11-02 09:39:26 -0600

    story 1

---
+++ spec/features/studios/index_spec.rb
@@ -0,0 +1,21 @@
+
+
+require "rails_helper"
+
+RSpec.describe "index", type: :feature do
+
+  before(:each) do
+    @universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+    @warnerbro = Studio.create!(name: 'Warner Brothers', location: 'Miami')
+
+    @ark = @universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+    @shrek = @universal.movies.create!(title: 'Shrek', creation_year: 2000, genre: 'Comedy')
+
+    visit "/studios"
+  end
+
+  it "story 1 - shows each studio's name and location" do
+    expect(page).to have_content("Universal Studios: Located in Hollywood")
+    expect(page).to have_content("Warner Brothers: Located in Miami")
+  end
+end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 RSpec.describe Actor, type: :model do
 
-  # describe 'validations' do
-  #   it { should validate_presence_of :name }
-  #   it { should validate_presence_of :age }
-  # end
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
+  end
 
   describe "relationships" do
     it { should have_many(:movie_actors) }
